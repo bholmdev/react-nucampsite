66ea0c20dacdcc427422a5a0d3c89f795c1600a1
Brandon Holman <bholmdev@gmail.com>
2022-12-10 13:28:32 -0800

    Week 2 Workshop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import './App.css';
 import Header from './components/Header';
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
+        <Route path="about" element={<AboutPage />}></Route>
         <Route path="directory" element={<CampsitesDirectoryPage />} />
         <Route 
           path="directory/:campsiteId" 
